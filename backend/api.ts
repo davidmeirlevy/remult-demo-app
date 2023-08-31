f9a287e94e47765823ca96ff8c6111934ae9ba92
David Meir-Levy <david@davidlevy.co.il>
2023-08-31 15:43:34 +0300

    step 3 - add the todo entity to the backend

--- backend/api.ts
+++ backend/api.ts
@@ -1,6 +1,11 @@
 import {remultFastify} from 'remult/remult-fastify';
+import {TodoEntity} from '../shared/Todo';
 
-const api = remultFastify()
+const api = remultFastify({
+  entities: [
+    TodoEntity
+  ]
+})
 
 
 export default api;
